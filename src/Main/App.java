10cfc11bb32ce867359f8b3473813c31235d0ee0
Eva-a-cat-style <belkinxvosteg@gmail.com>
2024-08-03 13:01:27 +0200

    Update app

---
+++ src/Main/App.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        int sum1 = 0;
+
+        for (int i = 1; i < 7; i++) {
+            sum1 += i;
+            System.out.println(i + ") Num is " + i + ", sum is " + sum1);
+        }
+        System.out.println("--------------------\n" + "Sum of number is " + sum1);
+    }}
+
+
+
